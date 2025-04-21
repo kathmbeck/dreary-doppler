b08d76e80d7c92f1ec608c488b861ffb56c6cc89
kathmbeck <katherine.murphy.beck@gmail.com>
2025-04-21 16:49:07 -0400

    try again

--- tracing.js
+++ tracing.js
@@ -1,7 +1,6 @@
 // tracing.js
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import pkg from '@opentelemetry/resources';
-const { Resource } = pkg;
+import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
@@ -9,7 +8,7 @@ import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 const sdk = new NodeSDK({
   traceExporter: new ConsoleSpanExporter(),
   spanProcessor: new SimpleSpanProcessor(new ConsoleSpanExporter()),
-  resource: new Resource({
+  resource: Resource.default.create({
     [SemanticResourceAttributes.SERVICE_NAME]: 'astro-tracing-demo',
   }),
 });
